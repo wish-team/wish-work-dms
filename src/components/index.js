d6667d397e708eb25bae539fa68dee201ed66052
Amirhosein Shirani <amirhhh.shirani@yahoo.com>
2021-03-18 21:10:07 +0330

    feat: export correctness

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,3 @@
-export * from "button/Button";
-export * from "assets/logo.svg";
-export * from "form/input/Input";
+export * from "./button/Button";
+export * from "./assets/logo.svg";
+export * from "./form/input/Input";
