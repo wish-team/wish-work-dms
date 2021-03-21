f14fde2780fdcc6e821bb1852c061ad460e8c8a2
Amirhosein Shirani <amirhhh.shirani@yahoo.com>
2021-03-21 18:08:09 +0330

    feat: Add button component --2

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,23 +1,30 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import './button.scss';
+import React from 'react'
+import PropTypes from 'prop-types'
+import './button.scss'
+
+interface Props {
+  primary: boolean,
+  backgroundColor: string,
+  size: string,
+  label: string
+}
 
 /**
  * Primary UI component for user interaction
  */
-export const Button = ({ primary, backgroundColor, size, label, ...props }) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+export const Button = ({ primary, backgroundColor, size, label, ...props }: Props) => {
+  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary'
   return (
     <button
       type="button"
       className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
-      style={backgroundColor && { backgroundColor }}
+      style={{ backgroundColor: backgroundColor }}
       {...props}
     >
       {label}
     </button>
-  );
-};
+  )
+}
 
 Button.propTypes = {
   /**
@@ -39,12 +46,12 @@ Button.propTypes = {
   /**
    * Optional click handler
    */
-  onClick: PropTypes.func,
-};
+  onClick: PropTypes.func
+}
 
 Button.defaultProps = {
   backgroundColor: null,
   primary: false,
   size: 'medium',
-  onClick: undefined,
-};
+  onClick: undefined
+}
